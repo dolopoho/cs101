a9dc542cb95b88fea1478516de77b0897f8c3628
dolopoho <hwd205@gmail.com>
2025-09-18 15:59:12 +0800

    Update main2.c

--- ex2/main2.c
+++ ex2/main2.c
@@ -1,1 +1,18 @@
+#inclue <studio.h>
+int main() {
+    char a = 5:
+    printf("\t%d\n", a&1);
+    printf("\t%d\n", a&2);
+    printf("\t%d\n", a|2);
+    return 0;
+}
 
+#inclue <studio.h>
+int main()  {
+    char a = 5;
+    print("\t%d\n", a^1);
+    printf("\t%d\n", ~a);
+    printf('\t%d\n', a>>1);
+    printf("\t%d\n", a<<1);
+    return 0;
+}
