339a876a4d67694555ab6046d1c0c62f5f1e7f4b
dolopoho <hwd205@gmail.com>
2025-09-18 15:54:57 +0800

    Update main1.c

--- ex2/main1.c
+++ ex2/main1.c
@@ -2,7 +2,7 @@
 int main() {
     char a = 5:
     printf("\t%d\n", a&1);
-    //0101 & 001 = 0001
     printf("\t%d\n", a&2);
-    //0101 & 0010 = 0000
+    printf("\t%d\n", a|2);
+    return 0;
 }
